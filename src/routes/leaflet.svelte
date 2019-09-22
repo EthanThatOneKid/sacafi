5a18a460c6a44655456940d4bc16ef99f4217c56
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2019-09-22 01:10:55 -0700

    i cant really figure out how to get a map to work in svelte...

---
+++ src/routes/leaflet.svelte
@@ -0,0 +1,29 @@
+<script>
+
+    // Dependencies
+    import Map from 'ol/Map';
+    import View from 'ol/View';
+    import TileLayer from 'ol/layer/Tile';
+    import XYZ from 'ol/source/XYZ';
+
+    new Map({
+        target: 'map',
+        layers: [
+            new TileLayer({
+                source: new XYZ({
+                    url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                })
+            })
+        ],
+        view: new View({
+            center: [0, 0],
+            zoom: 2
+        })
+    });
+
+</script>
+
+<div>
+    <h1>Hello World</h1>
+    <div id="map"></div>
+</div>
