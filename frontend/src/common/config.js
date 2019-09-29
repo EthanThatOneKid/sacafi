65745918eb4bcc4d3bc12290e1cbdd581b2ed7d1
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2019-09-29 01:37:30 -0700

    Update config.js

--- frontend/src/common/config.js
+++ frontend/src/common/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://conduit.productionready.io/api";
+export const API_URL = "http://127.0.0.1:3000"; // "https://conduit.productionready.io/api";
 export default API_URL;
