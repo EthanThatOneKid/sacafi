e2163ccd9eb7382d6342cac68da5f5b2426c6c02
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2019-10-12 16:05:57 -0700

    Create .eslintrc.js

---
+++ backend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    "extends": ["prettier", "plugin:node/recommended"],
+    "plugins": ["prettier"],
+    "rules": {
+        "prettier/prettier": "error",
+        "no-unused-vars": "warn",
+        "no-console": "off",
+        "func-names": "off",
+        "no-process-exit": "off",
+        "object-shorthand": "off",
+        "class-methods-use-this": "off",
+        "no-underscore-dangle": "warn",
+        "no-bitwise": "warn",
+        "no-param-reassign": "warn",
+        "no-underscore-dangle": "off"
+    }
+};
