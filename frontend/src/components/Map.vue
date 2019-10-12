8940134830208654e4e1f1d7cb635188e55494f5
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2019-10-12 16:16:59 -0700

    Create Map.vue

---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,68 @@
+<template>
+  <div style="height: 100%; width: 100%">
+    <div class="info" style="height: 15%">
+      <span>Center: {{ center }}</span>
+      <span>Zoom: {{ zoom }}</span>
+      <span>Bounds: {{ bounds }}</span>
+    </div>
+    <l-map
+      style="height: 50vh; width: 50vw"
+      :zoom="zoom"
+      :center="center"
+      @update:zoom="zoomUpdated"
+      @update:center="centerUpdated"
+      @update:bounds="boundsUpdated"
+    >
+      <v-geosearch :options="geosearchOptions"></v-geosearch>
+      <l-tile-layer :url="url"></l-tile-layer>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer } from "vue2-leaflet";
+import { OpenStreetMapProvider } from "leaflet-geosearch";
+import VGeosearch from "vue2-leaflet-geosearch";
+
+export default {
+  name: "Map",
+  components: {
+    LMap,
+    LTileLayer,
+    VGeosearch
+  },
+  data() {
+    return {
+      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      zoom: 3,
+      center: [47.41322, -1.219482],
+      bounds: null,
+      geosearchOptions: {
+        provider: new OpenStreetMapProvider()
+      }
+    };
+  },
+  methods: {
+    zoomUpdated(zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated(center) {
+      this.center = center;
+    },
+    boundsUpdated(bounds) {
+      this.bounds = bounds;
+    }
+  }
+};
+</script>
+
+<style>
+@import "~leaflet/dist/leaflet.css";
+@import "~leaflet-geosearch/assets/css/leaflet.css";
+.leaflet-fake-icon-image-2x {
+  background-image: url(../../node_modules/leaflet/dist/images/marker-icon-2x.png);
+}
+.leaflet-fake-icon-shadow {
+  background-image: url(../../node_modules/leaflet/dist/images/marker-shadow.png);
+}
+</style>
