cbc6ea887458f9916d16f20308edf64dffa7fcc2
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2019-10-12 15:57:47 -0700

    gdsghfdeatgf

--- frontend/src/common/config.js
+++ frontend/src/common/config.js
@@ -1,2 +1,7 @@
-export const API_URL = "https://conduit.productionready.io/api";
+// Initialize API URL; the root path
+// Note: Realworld example API URL is "https://conduit.productionready.io/api"
+const PROD_API_URL = "http://sacafi.com/api",
+  DEV_API_URL = "http://127.0.0.1:3000/api";
+export const API_URL =
+  process.env.NODE_ENV === "production" ? PROD_API_URL : DEV_API_URL;
 export default API_URL;
