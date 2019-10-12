2c49db0fc5c6dd62dbc83dfa8f32ce16d949b900
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2019-10-12 16:30:38 -0700

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,16 @@ import ApiService from "./common/api.service";
 import DateFilter from "./common/date.filter";
 import ErrorFilter from "./common/error.filter";
 
+// Resolve Leaflet issues
+import { Icon } from "leaflet";
+import "leaflet/dist/leaflet.css";
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
+
 Vue.config.productionTip = false;
 Vue.filter("date", DateFilter);
 Vue.filter("error", ErrorFilter);
