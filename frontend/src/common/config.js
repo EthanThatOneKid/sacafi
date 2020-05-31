9eaa7a0f1f7562ae05a50badd4b9c7c4e80faf10
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-05-31 01:24:52 -0700

    Update config.js

--- frontend/src/common/config.js
+++ frontend/src/common/config.js
@@ -1,6 +1,6 @@
 // Initialize API URL; the root path
 // Note: Realworld example API URL is "https://conduit.productionready.io/api"
-const PROD_API_URL = "http://sacafi.com/api",
+const PROD_API_URL = "http://sacafi.herokuapp.com/api",
   DEV_API_URL = "http://127.0.0.1:3000/api";
 export const API_URL =
   process.env.NODE_ENV === "production" ? PROD_API_URL : DEV_API_URL;
